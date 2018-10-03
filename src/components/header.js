297e1aad62bb81dc4e4e784326ae44cadd91c593
keegn <keegan.t.burkett@gmail.com>
2018-10-02 22:10:20 -0500

    header updates

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import logo from '../images/gatsby-icon.png'
 
 const Header = ({ siteTitle }) => (
-  <div>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={logo} alt="logo" width="30"></img></Link>
+      <Link to="/courses">Product</Link>
+      <Link to="/courses">Pricing</Link>
+      <Link to="/courses">About</Link>
+      <Link to="/courses">Get started</Link>
     </div>
   </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import Header from './header'
 
 import './layout.css'
 
@@ -27,7 +28,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-
+        <Header />
         <div>{children}</div>
       </>
     )}
