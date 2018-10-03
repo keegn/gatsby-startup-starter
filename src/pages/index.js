d6c5bd497b598a15b5499681f51024816195515a
keegn <keegan.t.burkett@gmail.com>
2018-10-02 22:54:16 -0500

    header progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+
 import Layout from '../components/layout'
 
 const IndexPage = () => (
@@ -9,7 +10,6 @@ const IndexPage = () => (
       <div className="HeroGroup">
         <h1>Intelligent mortgage software.</h1>
         <p>We are providing the world with the greatest new software ever.</p>
-        <Link to="/page-2/">Go to page 2</Link>
       </div>
     </div>
   </Layout>
