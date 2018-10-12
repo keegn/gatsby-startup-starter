9b6cb955d0c3fad7fb00eee4b94a836c30d5344b
keegn <keegan.t.burkett@gmail.com>
2018-10-11 23:28:45 -0500

    Updated hero copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => (
   <Layout>
     <Hero>
       <HeroGroup>
-        <HeroGroupH1>Intelligent mortgage software.</HeroGroupH1>
-        <HeroP>We are providing the world with the greatest new software ever.</HeroP>
+        <HeroGroupH1>Intelligent business software.</HeroGroupH1>
+        <HeroP>Reinventing business software with the help of smart machines.</HeroP>
         <HeroA href="/">Watch the video</HeroA>
       </HeroGroup>
     </Hero>
